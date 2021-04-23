632fa5d7f836d6ee5a7f14d488ed6a0284242c72
Geovane Machado <geovanemachado9@gmail.com>
2021-04-23 14:58:36 -0300

    Conta Vogais

--- lista07/ex02.py
+++ lista07/ex02.py
@@ -1,1 +1,11 @@
-def vogais()
+def vogais(a):
+    a = a.upper()
+    count = 0
+    for i in a:
+        if i in 'AEIOU':
+            count += 1
+    return f'{count} vogais'
+
+    
+name = str(input())
+print(vogais(name))
