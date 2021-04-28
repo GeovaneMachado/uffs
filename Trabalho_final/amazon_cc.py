548c728c5602efd15a8bb5e508289b43770e1ee7
Geovane Machado <geovanemachado9@gmail.com>
2021-04-28 20:20:17 -0300

    bem vindo 1.1

--- Trabalho_final/amazon_cc.py
+++ Trabalho_final/amazon_cc.py
@@ -1,11 +1,16 @@
 def bem_vindo():
     from time import sleep
-    print('*=' *15)
+    cabe = '*=' *15
     comeco = 'Bem vindo a loja virtual'
+    for i in cabe:
+        print(i, end='')
+        sleep(0.25)
     print('|  ', end='')
     for i in comeco:
         print(i, end='')
         sleep(0.25)
-    print(f'{"|":>4}')
-    print('*=' *15)
+    print(f'{"|":>3}')
+    for i in cabe:
+        print(i, end='')
+
 bem_vindo()
