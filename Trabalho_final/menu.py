404bef6aed9960eb181464a76adcc39636c2da54
lucasschmidt015 <lucas.schmidt015@gmail.com>
2021-04-28 22:47:11 -0300

    remuve_testes

--- Trabalho_final/menu.py
+++ Trabalho_final/menu.py
@@ -5,6 +5,5 @@ def menu():
         Menu=int(input("[1]-Cadastro\n[2]-Login\n[3]-consultar cliente\n[4]-Fazer compras\n[5]-Ver carrinho\n[6]-pagamento\n"))
     return Menu 
 
-teste=menu()
-print(teste)
+
 
