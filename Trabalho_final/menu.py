31b75c4537035eadfe94004291b233dbfda8b1e4
Geovane Machado <geovanemachado9@gmail.com>
2021-04-29 22:29:39 -0300

    +1

--- Trabalho_final/menu.py
+++ Trabalho_final/menu.py
@@ -1,3 +1,20 @@
+def bem_vindo():
+    from time import sleep
+    cabe = '*=' *15
+    comeco = 'Bem vindo a loja virtual'
+    for i in cabe:
+        print(i, end='')
+        sleep(0.1)
+    print()
+    print('|  ', end='')
+    for i in comeco:
+        print(i, end='')
+        sleep(0.25)
+    print(f'{"|":>3}')
+    for i in cabe:
+        print(i, end='')
+        sleep(0.1)
+        
 def menu():
     Menu=int(input("[1]-Cadastro\n[2]-Login\n[3]-consultar cliente\n[4]-Fazer compras\n[5]-Ver carrinho\n[6]-pagamento\n"))
     while Menu > 6 or Menu < 1:
