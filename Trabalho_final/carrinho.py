f598985c72b89c10607bf1655127f68511f1fa7c
lucasschmidt015 <lucas.schmidt015@gmail.com>
2021-05-05 22:06:19 -0300

    carrinho

--- Trabalho_final/carrinho.py
+++ Trabalho_final/carrinho.py
@@ -1,5 +1,7 @@
 def prod():
-    mercearia = ['[1]-feijão 1kg',7.99,'[2]-arroz 5kg',19.99,'[3]-sal 1kg',3.99,'[4]-açucar 5kg',10.99,'[5]-farinha 5kg',14.99,'[6]-fermento 200g',4.99]
+    mercearia = [{'produto':'feijão 1kg','valor':7.99},{'produto':'arroz 5kg','valor':19.99},
+    {'produto':'sal 1kg','valor':3.99},
+    {'produto': 'açucar 5kg','valor':10.99},{'produto':'farinha 5kg','valor':14.99},{'produto':'fermento 200g','valor':4.99}]
     eletronico = ['[1]-Fone de ouvido',79.99, '[2]-mouse',99.99,'[3]-teclado',139.99,'[4]-carregador',29.99,'[5]-webcam',99.99]
     bebidas = ['[1]-cerveja 350ml',2.49,'[2]-agua de coco 1l',5.99,'[3]-Refrigerante 2l',7.99,'[4]-Suco 1l',4.99]
     segmento = int(input('Segmetno:\n[1] mercearia\n[2] Bebidas\n[3] Eletronicos\nOpcao: '))
@@ -18,34 +20,39 @@ def prod():
 
 def carrinho(produtos):
     cesto=[]
+    total=[]
     add=None
     cont="s"
-    for i in produtos:
-        print(i)
+    for j in produtos:
+        for k,v in j.items():
+            print(f" {k}  {v} ",end=" ")
+        print()
     while True:
         add=int(input("Qual produto vc deseja adicionar ao carrinho? "))
         if add == 1:
             cesto.append(produtos[0])
-            cesto.append(produtos[1])
+            total.append(produtos[1])
         elif add == 2:
             cesto.append(produtos[2])
-            cesto.append(produtos[3])
+            total.append(produtos[3])
         elif add == 3:
             cesto.append(produtos[4])
-            cesto.append(produtos[5])
+            total.append(produtos[5])
         elif add == 4:
             cesto.append(produtos[6])
-            cesto.append(produtos[7])      
+            total.append(produtos[7])      
         elif add == 5:
             cesto.append(produtos[8])
-            cesto.append(produtos[9])
+            total.append(produtos[9])
         elif add == 6:
             cesto.append(produtos[10])
-            cesto.append(produtos[11])
+            total.append(produtos[11])
+        print(cesto)
+        print(total)
         cont=input("Quer adicionar mais produtos? [s/n]")
         if cont == "n":
             break
-    print(cesto)
+    return cesto
 
 
 
