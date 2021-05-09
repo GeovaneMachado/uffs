a585c1c980ba372304c231d74880afe26f05a827
lucasschmidt015 <lucas.schmidt015@gmail.com>
2021-05-08 21:29:56 -0300

    menu1e2

--- Trabalho_final/Matriz.py
+++ Trabalho_final/Matriz.py
@@ -29,11 +29,18 @@ def titulo(palavra):
     print(f'{palavra.upper()}'.center(30))
     print(f'{"-"*30}')
 
-def menu():
-    Menu=int(input("[1]-Cadastro\n[2]-Login\n[3]-consultar cliente\n[4]-Fazer compras\n[5]-Ver carrinho\n[6]-pagamento\n"))
-    while Menu > 6 or Menu < 1:
+def menu1():
+    Menu=int(input("[1]-Cadastro\n[2]-Login\n"))
+    while Menu > 2 or Menu < 1:
         print("Opção invalida")
-        Menu=int(input("[1]-Cadastro\n[2]-Login\n[3]-consultar cliente\n[4]-Fazer compras\n[5]-Ver carrinho\n[6]-pagamento\n"))
+        Menu=int(input("[1]-Cadastro\n[2]-Login\n"))
+    return Menu
+
+def menu2():
+    Menu=int(input("[1]-consultar cliente\n[2]-Fazer compras\n[3]-Ver carrinho\n[4]-pagamento\n[5]-Voltar\n"))
+    while Menu > 5 or Menu < 1:
+        print("Opção invalida")
+        Menu=int(input("[1]-consultar cliente\n[2]-Fazer compras\n[3]-Ver carrinho\n[4]-pagamento\n[5]-Voltar\n"))
     return Menu
 
 def valida_cpf(cpf):
@@ -152,6 +159,67 @@ def cons(x):
         else:
             print("CPF não encontrado!")
 
+def prod():#produtos cadastrados
+    mercearia = [{'produto':'feijão 1kg','valor':7.99}, {'produto':'arroz 5kg','valor':19.99},{'produto':'sal 1kg','valor':3.99},
+    {'produto': 'açucar 5kg','valor':10.99},{'produto':'farinha 5kg','valor':14.99},{'produto':'fermento 200g','valor':4.99}]
+    eletronico = [{'produto':'Fone de ouvido','valor':79.99}, {'produto': 'mouse','valor':99.99},{'produto':'teclado','valor':139.99},
+    {'produto': 'carregador','valor':29.99}, {'produto': 'webcam','valor':99.99}]
+    bebidas = [{'produto': 'cerveja 350ml', 'valor': 2.49},{'produto': 'agua de coco 1l','valor': 5.99},
+    {'produto':'Refrigerante 2l','valor':7.99},{'produto':'Suco 1l','valor':4.99}]
+    segmento = int(input('Segmento:\n[1] mercearia\n[2] Bebidas\n[3] Eletronicos\nOpcao: '))
+    while segmento > 4 or segmento < 1:
+        print()
+        print("OPÇÃO INVALIDA")
+        print()
+        segmento = int(input('Segmento:\n[1] mercearia\n[2] Bebidas\n[3] Eletronicos\n[4]Voltar\nOpcao: '))
+    if segmento == 1: # retorna qual segmento a pessoa quer ver para fazer a compra
+        return mercearia
+    elif segmento == 2:
+        return bebidas
+    elif segmento == 3:
+        return eletronico
+    else:
+        return 'Voltar'
+
+
+def carrinho(produtos):
+    cesto=[]
+    adic_car=None
+    cont="s"
+    for i, v in enumerate(produtos):
+        print(f" {i} {v['produto']} R${v['valor']} ")#mostra os produtos formatado
+    print()
+    valor_final = 0
+    while True:
+        try:
+            adic_car=int(input("Digite o codigo do produto:  "))
+            cesto.append(produtos[adic_car])
+            cesto[-1]['quantidade'] = int(input('Quantidade do produto: '))
+            valor_final += cesto[-1]['valor'] * cesto[-1]['quantidade'] #soma o valor que vai da do produto e acrescenta no valor total
+            cont=input("Quer adicionar mais produtos? [s/n]").lower()
+            while cont not in 'sn':
+                cont = input('Quer visualizar o valor total?[s/n]: ').lower()
+        except:
+            print('ERRO!!')
+        if cont == "n":
+            break
+    cesto.append({'VALOR_TOT':valor_final})
+    return cesto
+    
+
+def ver_carrinho(compras):
+    for i, v in enumerate(compras): #mostra os produtos adicionados no carrinho
+        try:
+            print(f'Item: {i} {v["produto"]}  R${v["valor"]:.2f}    Quantidade:{v["quantidade"]}')
+        except:
+            pass
+    while True:
+        vis_tot = input('Quer visualizar o valor total?[s/n]: ').lower()
+        if vis_tot in 'sn':
+            break
+    if vis_tot == 's':
+        print(f'Valor total: R${compras[-1]}')
+
     
   
   
@@ -167,8 +235,10 @@ def cons(x):
 
 #bem_vindo()
 tot_cad = []
+compras = []
+count = soma = 0
 while True:
-    start = menu()
+    start = menu1()
     if start == 1: #Cadastro de clientes novos
         titulo('CADASTRO')
         registro = main()
@@ -184,8 +254,24 @@ while True:
             tot_cad += [registro]
     if start == 2:  # Login 
         loguin=log(tot_cad)
-    if start == 3: # Consultar cliente 
-        consulta=cons(tot_cad)
-        print("Cadastro encontrado")
-        print(f"nome: {consulta[0]}\nE-mail: {consulta[1]}")
-    if start == 4: # função destinada a fazer compras 
+        while True:
+            play=menu2()
+            if play == 1: # Consultar cliente 
+                consulta=cons(tot_cad)
+                print("Cadastro encontrado")
+                print(f"nome: {consulta[0]}\nE-mail: {consulta[1]}")
+            if play == 2: # função destinada a fazer compras 
+                itens = prod()
+                if itens == 'Voltar':
+                    print('Voltando para o menu')
+                    break
+                produtos = carrinho(itens)
+                compras += produtos
+                soma += compras[-1]['VALOR_TOT']
+                compras.remove(compras[-1])
+                compras.append(soma)
+            if play == 3: 
+                ver_carrinho(compras)
+            
+        
+        
