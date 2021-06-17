10f8ca7362bf0862d1fa15726198d98b9eb1f48c
GeovaneMachado <73132514+GeovaneMachado@users.noreply.github.com>
2021-06-17 16:43:04 -0300

    Pasta estruturas de dados

---
+++ estrutura_dados/lista1_uri/soma_var.c
@@ -0,0 +1,14 @@
+/*Leia dois valores inteiros, no caso para variáveis A e B.
+ A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.*/
+ #include<stdio.h>
+
+ int main()
+ {
+     int A, B, SOMA;
+     scanf("%d", &A);
+     scanf("%d", &B);
+     SOMA = A + B;
+     printf("SOMA = %i", SOMA);
+     printf("\n");
+     return 0;
+ }
